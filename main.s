602d1f13ffb614fe316f40dfc63f6386a0d3671e
Henrique Valente <bravehenrique@gmail.com>
2025-01-31 14:36:41 -0300

    retira as funções não usadas na pasta helpers

--- main.s
+++ main.s
@@ -2,7 +2,7 @@
 .globl _start
 .globl tocar_musica  # Declare a função 'tocar_musica' como global para que o linker saiba que existe
 
-.include "helpers/music.s"  # Incluir o arquivo music.s
+.include "helpers/music.s"      # Incluir o arquivo music.s
 
 _start:
     # Chama a função que toca a música
